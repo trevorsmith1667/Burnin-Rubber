5857921d09e86195edaa0e386bffd33d7762b5bc
Trevor Smith <trevor.smith1667@gmail.com>
2021-04-07 16:45:20 -0700

    starting car movements

--- game.js
+++ game.js
@@ -11,15 +11,14 @@ document.addEventListener('keyup', pressKeyOff);
 
 
 //Game variables
-let animationGame = requestAnimationFrame(playGame);
 let gamePlay = false;
 let player 
 let keys = {
-    arrowUp: false, 
-    arrowDown: false,
-    arrowLeft: false,
-    arrowRight: false
-
+    ArrowUp: false, 
+    ArrowDown: false,
+    ArrowLeft: false,
+    ArrowRight: false
+    
 }
 function startGame(){
     console.log(gamePlay);
@@ -30,29 +29,52 @@ function startGame(){
     div.y = 500
     container.appendChild(div);
     gamePlay = true;
+    animationGame = requestAnimationFrame(playGame);
     player = {
+        ele: div,
         speed: 1,
         lives: 3,
-        gameScore: 0,
-        neededPasses: 10
+        score: 0,
+        neededPasses: 10,
+        roadWidth: 250,
+    }
+    startBoard();
+}
+function startBoard(){
+    for(let x = 0;x< 13; x++){
+        let div = document.createElement('div')
+        div.setAttribute('class', 'road')
+        div.style.top = (x*50) + 'px';
+        container.appendChild(div)
     }
 }
-
-function pressKeyOn(){
-
+function pressKeyOn(event){
+    event.preventDefault();
+    console.log(keys);
+    keys[event.key] = true
 }
 
-function pressKeyOff(){
+function pressKeyOff(event){
+    event.preventDefault();
+    console.log(keys);
+    keys[event.key] = false
+
 
 }
 
 function updateDash(){
-    console.log(player)
+    // console.log(player)
+    score.innerHTML = player.score;
+    lives.innerHTML = player.lives;
+    speedo.innerHTML = player.speed;
 }
 
 function playGame(){
     if(gamePlay){
-    updateDash()
+        updateDash()
+        if (keys.ArrowUp){
+            console.log(player.ele.x)
+        }
     }
 animationGame = requestAnimationFrame(playGame)
 }
